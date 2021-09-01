938faacbed62391721e70ab2d6ed9e1b6273f61a
isaacy2012 <50868057+isaacy2012@users.noreply.github.com>
2021-09-01 23:27:37 +1200

    Fixed issue #1
    - Detects whether the string is ALL_UPPER_CASE
    - If it is, splits by "_" and converts each String to Title Case
    - Joins together with joinToString

--- src/main/kotlin/com/kdocer/generator/KDocGenerator.kt
+++ src/main/kotlin/com/kdocer/generator/KDocGenerator.kt
@@ -17,7 +17,20 @@ interface KDocGenerator {
 
     fun StringBuilder.appendLine(text: String): StringBuilder = append(text).append(LF)
 
-    fun nameToPhrase(name:String):String{
+
+    /**
+     * @param name the name
+     * @return the result of converting the name to a phrase
+     */
+    private fun nameToPhraseAllUpperCase(name: String): String {
+        return name.split(Pattern.compile("_"))
+            .joinToString(separator = " ") { s: String -> s.toTitleCase() }
+    }
+
+    fun nameToPhrase(name: String): String {
+        if (name.isAllUpperCase()) {
+            return nameToPhraseAllUpperCase(name)
+        }
         val array = name.split(Pattern.compile("(?<=[a-zA-Z])(?=[A-Z])"))
         val builder = StringBuilder()
         array.forEach {
@@ -27,5 +40,30 @@ interface KDocGenerator {
         val phrase = builder.toString()
         return phrase.capitalize()
     }
+}
+
+/**
+ * Converts a String to Title Case
+ * @return the String in Title Case
+ */
+private fun String.toTitleCase(): String {
+    val sb = StringBuilder()
+    this.forEachIndexed { i: Int, c: Char ->
+        sb.append(if (i == 0) {c.toUpperCase()} else {c.toLowerCase()})
+    }
+    return sb.toString()
+}
+
+/**
+ * Checks whether a String is all uppercase, by iterating through all the characters
+ * @return whether the String is all uppercase
+ */
+private fun String.isAllUpperCase(): Boolean {
+    this.forEach {
+        if (it.isLowerCase()) {
+            return false
+        }
+    }
+    return true
 }
 
